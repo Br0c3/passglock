104abadf09089a4ee6ce460d896f11a5eab68cef
br0c3 <zounhopeudro@gmail.com>
2024-02-19 21:59:52 +0100

    initialisation du projet django

---
+++ passlock/passManage/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('genpass', views.genpass, name="genpass"),
+    path('encod', views.encod, name="encod"),
+    path('decod', views.decod, name="decod"),
+    path('openfil', views.openfil, name="openfil"),
+    path('managefil', views.managefil, name="managefil"),
+    path('addata', views.addata, name="addata"),
+    path('editdata', views.editdata, name="editdata")
+]
